2ff35c510b485dde93a57a27a61463c6f1482880
Pocchi <pocchi13.tr@gmail.com>
2020-03-14 19:07:27 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,66 @@
 import React, { Component } from 'react';
 import './App.scss';
 import logo from './logo.png';
+import moment from 'moment';
+moment.locale('ja');
 
-class App extends Component {
+const NOW_DATE: moment.Moment = moment();
+const START_MONTH: moment.Moment = moment().startOf('month');
+const END_MONTH: moment.Moment = moment().endOf('month');
+const START_WEEK: moment.Moment = moment(START_MONTH).startOf('week');
+const END_WEEK: moment.Moment = moment(END_MONTH).endOf('week');
+const BEFORE_MONTH: moment.Moment = moment().add(1, 'month');
+const AFTER_MONTH: moment.Moment = moment().add(-1, 'month');
+
+let tmpDay: moment.Moment = START_WEEK;
+let calendarDays = [];
+let tmpDays: moment.Moment[] = [];
+
+while(moment(tmpDay).isSameOrBefore(END_WEEK)) {
+  if (tmpDays.length == 7) {
+    tmpDays.push(tmpDay);
+    calendarDays.push(tmpDays);
+    tmpDays = [];
+  } else {
+    tmpDays.push(tmpDay);
+  }
+  moment(tmpDay).add(1, 'days');
+}
+console.log(calendarDays);
+
+interface AppState {
+  currentDate: Date;
+  nowDate: Date;
+  startMonth: Date;
+  startWeek: Date;
+  endMonth: Date;
+  endWeek: Date;
+  beforeMonth: Date;
+  afterMonth: Date;
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+  }
   render() {
     const logoAttrs = {
       alt: 'logo',
       className: 'App-logo',
       src: logo,
     };
-    const title = 'こんにちはReact';
-    const targets = ['World', 'Kanae', 'Yukina'];
 
     return (
       <div className="App">
         <header className="App-header">
-          {
-            // コメントはこう書く
-          }
           <img {...logoAttrs} />
-          {title && <p>{title}</p>}
-          {targets.map(target => (
-            <p>Hello, {target}!</p>
-          ))}
         </header>
+        <main>
+            <div className="Calendar">
+                <table>
+                </table>
+            </div>
+        </main>
       </div>
     );
   }
