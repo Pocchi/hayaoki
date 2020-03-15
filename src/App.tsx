23c92c0ac8587229a47f6f8edba8a41a369977e4
Pocchi <pocchi13.tr@gmail.com>
2020-03-15 11:01:31 +0900

    ひとまずカレンダー表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,86 @@
 /* eslint-disable react/jsx-key, react/jsx-props-no-spreading, jsx-a11y/alt-text */
-import React, { Component } from 'react';
+import React, { FC, useState } from 'react';
 import './App.scss';
 import logo from './logo.png';
 import moment from 'moment';
 moment.locale('ja');
 
 const NOW_DATE: moment.Moment = moment();
-const START_MONTH: moment.Moment = moment().startOf('month');
-const END_MONTH: moment.Moment = moment().endOf('month');
-const START_WEEK: moment.Moment = moment(START_MONTH).startOf('week');
-const END_WEEK: moment.Moment = moment(END_MONTH).endOf('week');
-const BEFORE_MONTH: moment.Moment = moment().add(1, 'month');
-const AFTER_MONTH: moment.Moment = moment().add(-1, 'month');
+const getCurrentDate = (date: moment.Moment) => moment(date);
+const getAfterMonth = (date: moment.Moment) => moment(date).add(1, 'month');
+const getBeforeMonth = (date: moment.Moment) => moment(date).add(-1, 'month');
 
-let tmpDay: moment.Moment = START_WEEK;
-let calendarDays = [];
-let tmpDays: moment.Moment[] = [];
-
-while(moment(tmpDay).isSameOrBefore(END_WEEK)) {
-  if (tmpDays.length === 7) {
-    tmpDays.push(tmpDay);
-    calendarDays.push(tmpDays);
-    tmpDays = [];
-  } else {
-    tmpDays.push(tmpDay);
+const createCalendar = (date: moment.Moment) => {
+  const startMonth = moment(date).startOf('month');
+  const endMonth = moment(date).endOf('month');
+  const startWeek: moment.Moment = moment(startMonth).startOf('week');
+  const endWeek: moment.Moment = moment(endMonth).endOf('week');
+  let tmpDay: moment.Moment = startWeek;
+  let calendarDays: string[][] = [];
+  let tmpDays: string[] = [];
+  while(moment(tmpDay).isSameOrBefore(endWeek)) {
+    if (tmpDays.length === 6) {
+      tmpDays.push(moment(tmpDay).format('YYYY-MM-DD'));
+      calendarDays.push(tmpDays);
+      tmpDays = [];
+    } else {
+      tmpDays.push(moment(tmpDay).format('YYYY-MM-DD'));
+    }
+    tmpDay.add(1, 'days');
   }
-  tmpDay.add(1, 'days');
-}
-console.log(calendarDays);
-console.log(BEFORE_MONTH);
-console.log(NOW_DATE);
-console.log(AFTER_MONTH);
-
-interface AppState {
-  currentDate: Date;
-  nowDate: Date;
-  startMonth: Date;
-  startWeek: Date;
-  endMonth: Date;
-  endWeek: Date;
-  beforeMonth: Date;
-  afterMonth: Date;
+  return calendarDays;
 }
 
-class App extends Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    console.log('constructor')
-  }
-  render() {
-    const logoAttrs = {
-      alt: 'logo',
-      className: 'App-logo',
-      src: logo,
-    };
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img {...logoAttrs} />
-        </header>
-        <main>
-            <div className="Calendar">
-                <table>
-                </table>
-            </div>
-        </main>
-      </div>
-    );
-  }
-}
+const App: FC = () => {
+  const [currentDate, setCurrentDate] = useState(NOW_DATE); // ページ送り用
+  const [selectDate, setSelectDate] = useState(NOW_DATE); // モーダル表示用
+  const nowDate = NOW_DATE;
+  const beforeMonth = getBeforeMonth(currentDate);
+  const afterMonth = getAfterMonth(currentDate);
+  const days = createCalendar(currentDate);
+  const onClickDate = (date: string) => () => {
+    // ページ送りはしない
+    setSelectDate(moment(date));
+  };
+  const onClickMonth = (date: string) => () => {
+    setCurrentDate(moment(date));
+  };
+  const logoAttrs = {
+    alt: 'logo',
+    className: 'App-logo',
+    src: logo,
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img {...logoAttrs} />
+      </header>
+      <main>
+          <div className="Calendar">
+              <table>
+                <tbody>
+                  <tr>
+                    {['日', '月', '火', '水', '木', '金', '土'].map((weekday, index) => (
+                      <th key={index}>{weekday}</th>
+                    ))}
+                  </tr>
+                  {days.map((week, key) => (
+                    <tr key={key}>
+                      {week.map((day, index) => (
+                        <td key={`${key}-${index}`}>
+                          <button onClick={onClickDate(day)}>
+                            {moment(day).format('YYYY-MM-DD')}
+                          </button>
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+          </div>
+      </main>
+    </div>
+  );
+};
 
 export default App;
